c38aeb6be351376bb7dfcce88f199a8b32971525
Daniel Xu <xudh47@gmail.com>
2023-06-04 23:32:10 -0700

    Updated to async for consistency

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,20 +2,23 @@ const { User } = require("../models");
 
 module.exports = {
   // GET all users
-  getUsers(req, res) {
-    User.find()
-      .select("-__v")
-      .then((userData) => res.json(userData))
-      .catch((err) => res.status(500).json(err));
+  async getUsers(req, res) {
+    try {
+      const userData = await User.find().select("-__v");
+      res.json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   },
 
   // Create a new user
-  createUser({ body }, res) {
-    User.create(body)
-      .then((userData) => res.json(userData))
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+  async createUser({ body }, res) {
+    try {
+      const userData = await User.create(body);
+      res.json(userData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
   },
 };
